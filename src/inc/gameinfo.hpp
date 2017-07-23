f5189cd711a2a3493f28824899004cb2fceb0c07
singaurav <gaurav.singh.00789@gmail.com>
2017-07-23 18:57:39 +0530

    Add documentation.

--- src/inc/gameinfo.hpp
+++ src/inc/gameinfo.hpp
@@ -8,6 +8,7 @@
 
 class AbstractGame {
 public:
+  // need these two functions to be able to stream games to and from a file
   virtual bool from_lines(const std::vector<std::string>) = 0;
   virtual std::vector<std::string> to_lines() = 0;
 };
@@ -30,25 +31,42 @@ struct Move {
 
 class Game : public AbstractGame {
 public:
+  // unique id for each game
   int id;
+  // white won 1-0, black won 0-1 or draw 1/2-1/2
   std::string result;
+  // total number of half moves in the game
   int ply_count;
+  // elo of the white player
   int white_elo;
+  // elo of the black player
   int black_elo;
+  // moves played in the game
   std::vector<Move> moves;
 
   bool from_lines(std::vector<std::string>);
   std::vector<std::string> to_lines();
 };
 
-enum MoveSampleStrategy { RANDOM, NORMAL };
-enum CountSampleStrategy { EXACTLY_N, PERC };
+enum MoveSampleStrategy {
+  // sample at random
+  RANDOM,
+  // sample according to a normal distribution
+  NORMAL
+};
+enum CountSampleStrategy {
+  // sample exactly a fixed number of moves.
+  EXACTLY_N,
+  // sample a percentage of total number of moves in the game.
+  PERC
+};
 
 class GameWithSampledMoves : public Game {
 public:
   GameWithSampledMoves() {}
   GameWithSampledMoves(Game const &g) : Game(g) {}
 
+  // move vector indexes of the sampled moves
   std::vector<unsigned> sampled_moves_indexes;
 
   template <CountSampleStrategy Strategy, typename Limit>
