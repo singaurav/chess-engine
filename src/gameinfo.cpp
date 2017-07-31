3674c957f70245375b6fb7d65a0e0062f2b5f9db
Jitesh Khandelwal <jk231092@gmail.com>
2017-07-31 22:45:06 +0530

    add spaces to get rid of compile error

--- src/gameinfo.cpp
+++ src/gameinfo.cpp
@@ -168,7 +168,7 @@ template <> void GameWithSampledMoves::sample_moves<RANDOM>(unsigned count) {
 
   std::uniform_int_distribution<int> d(0, moves.size() - 1);
   std::vector<unsigned> indexes =
-      Utils::sample_indices<std::uniform_int_distribution<int>>(d, moves.size(),
+      Utils::sample_indices<std::uniform_int_distribution<int> >(d, moves.size(),
                                                                 count);
   for (unsigned i = 0; i < count; ++i)
     sampled_moves_indexes.push_back(indexes[i]);
@@ -179,7 +179,7 @@ template <> void GameWithSampledMoves::sample_moves<NORMAL>(unsigned count) {
   std::normal_distribution<double> d =
       Utils::generate_normal_distribution(moves.size());
   std::vector<unsigned> indexes =
-      Utils::sample_indices<std::normal_distribution<double>>(d, moves.size(),
+      Utils::sample_indices<std::normal_distribution<double> >(d, moves.size(),
                                                               count);
   for (unsigned i = 0; i < count; ++i)
     sampled_moves_indexes.push_back(indexes[i]);
